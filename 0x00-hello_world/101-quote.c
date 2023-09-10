5316d3ccbb77248bdb447192fca5832ec3e29d35
ahmed <ahmedabdulla2010@live.com>
2023-09-10 10:46:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A c program that prints a line to the standard error
- * Return 1 (Success)
+ * main - A C program that prints a line to the standard error
+ * Return: 1 (Error)
  */
 int main(void)
 {
-	fput("and that piece of art is uneful\" - Dora korpar, 2015-10-19\n",stdout);
-	return(1);
+    write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
 }
+
