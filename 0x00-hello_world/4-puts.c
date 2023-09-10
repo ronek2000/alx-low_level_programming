5ce0d9e394d8f2d7692b0f9949b854b59eb96a5f
ahmed <ahmedabdulla2010@live.com>
2023-09-10 09:31:00 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts functio
+n
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"programming is like buileding a puzzle");
+	return (0);
+}
